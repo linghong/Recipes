dc8c158abad415a4f11267ac0fa1be2b05030c82
linghong <linghong_chen@hotmail.com>
2016-10-21 17:52:26 -0400

    added basic file structure and nav-bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rb works!';
+ 
 }
