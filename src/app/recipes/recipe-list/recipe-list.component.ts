cc6901ed8e1a2c3a92ffc5561e5a09fd08334ef2
linghong <linghong_chen@hotmail.com>
2016-10-27 15:37:07 -0400

    added *ngFor directory to loop through the list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe';
-import {RecipeItemComponent} from './recipe-item.component'
+import {RecipeItemComponent} from './recipe-item.component';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -9,10 +9,13 @@ import {RecipeItemComponent} from './recipe-item.component'
 
 export class RecipeListComponent implements OnInit {
 	
-	recipes: Recipe[]=[];
+	recipes: Recipe[]=[
+        new Recipe("Mediterranean Chicken and Pasta", "Mediterranean pasta made with chicken, artichoke hearts, garlic and dried crushed oregano", "http://images.meredith.com/fitness/images/recipe/ss_R137043.jpg", []),
+        new Recipe("Shrimp Scampi Bake", "wonderful 'zip' of Dijon-style mustard", "http://images.media-allrecipes.com/userphotos/560x315/808176.jpg", [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-	recipe = new Recipe("Mediterranean Chicken and Pasta", "Mediterranean pasta made with chicken, artichoke hearts, garlic and dried crushed oregano", "http://images.meredith.com/fitness/images/recipe/ss_R137043.jpg");
+
   
 	constructor() { }
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RecipeListComponent} from './recipe-list/recipe-list.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {Recipe} from './recipe'
+
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html'
